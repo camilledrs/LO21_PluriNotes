8265a65b11cc986c3ed77c3675e842967bbe47d2
CapucinePr <capucine.prudhomme@gmail.com>
2017-04-16 18:20:46 +0200

    Update LO21.cpp

--- LO21.cpp
+++ LO21.cpp
@@ -20,3 +20,19 @@ void NoteManager::addNote(Note* n){
 	}
 	note[nbNotes++]=n;
 }
+
+
+void RelationManager::addRelation(Relation* r){
+	for(unsigned int i=0; i<nbRelations; i++){
+		if (relations[i]->getId()==r->getId()) throw NoteException("error, creation of an already existent note");
+	}
+	if (nbRelations==nbMaxRelations){
+		Relation** newRelations= new Relation*[nbMaxRelations+5];
+		for(unsigned int i=0; i<nbRelations; i++) newRelations[i]=relations[i];
+		Relation** oldRelations=relation;
+		relation=newRelations;
+		nbMaxRelations+=5;
+		if (oldRelations) delete[] oldRelations;
+	}
+	relation[nbRelations++]=r;
+}

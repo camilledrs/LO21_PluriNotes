78744ee5992b8db7a4a830a423dac8fa2f8a98a4
CapucinePr <capucine.prudhomme@gmail.com>
2017-04-16 18:15:02 +0200

    Update LO21.cpp

--- LO21.cpp
+++ LO21.cpp
@@ -1,1 +1,22 @@
 #include "LO21.h"
+#include <QFile>
+#include <QTextCodec>
+#include <QtXml>
+#include <QMessageBox>
+
+
+
+void NoteManager::addNote(Note* n){
+	for(unsigned int i=0; i<nbNotes; i++){
+		if (notes[i]->getId()==n->getId()) throw NoteException("error, creation of an already existent note");
+	}
+	if (nbNotes==nbMaxNotes){
+		Note** newNotes= new Note*[nbMaxNotes+5];
+		for(unsigned int i=0; i<nbNotes; i++) newNotes[i]=notes[i];
+		Note** oldNotes=note;
+		note=newNotes;
+		nbMaxNotes+=5;
+		if (oldNotes) delete[] oldNotes;
+	}
+	note[nbNotes++]=n;
+}

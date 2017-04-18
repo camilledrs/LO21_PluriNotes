b853f90a2e1fc1e2c5abb6c8f88019966774abc1
Camille <camille.dresse@gmail.com>
2017-04-18 19:24:01 +0200

    Create NoteManager.h

---
+++ NoteManager.h
@@ -0,0 +1,48 @@
+#ifndef NOTESMANAGER_H_INCLUDED
+#define NOTESMANAGER_H_INCLUDED
+#include <ctime>
+#include <QString>
+#include "Notes.h"
+
+class NoteManager
+{
+    Note** notes;
+    unsigned int nbNotes;
+    unsigned int nbMaxNotes;
+    void addNote(Note* n);
+    
+    
+    NoteManager();
+    ~NoteManager();
+    NoteManager(const NoteManager& m);
+    NoteManager& operator=(const NoteManager& m);
+    
+    public :
+    class Iterator {
+            friend class NoteManager;
+            Note** currentN;
+            unsigned int nbRemain;
+            Iterator(Note** n, unsigned nb):currentN(n),nbRemain(nb){}
+        public:
+            Iterator():nbRemain(0),currentN(nullptr){}
+            bool isDone() const { return nbRemain==0; }
+            void next() {
+                if (isDone())
+                    throw NoteException("error, next on an iterator which is done");
+                nbRemain--;
+                currentN++;
+            }
+            Note& current() const {
+                if (isDone())
+                    throw NoteException("error, indirection on an iterator which is done");
+                return **currentN;
+            }
+};
+    
+    Iterator getIterator() {
+            return Iterator(notes,nbNotes);
+        }
+};
+
+
+#endif // NOTESMANAGER_H_INCLUDED

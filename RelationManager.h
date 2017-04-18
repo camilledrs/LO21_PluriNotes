d1cb2b1b5e8acc2bdf49720b0b26b8238f9a9706
Camille <camille.dresse@gmail.com>
2017-04-18 19:28:56 +0200

    Create RelationManager.h

---
+++ RelationManager.h
@@ -0,0 +1,47 @@
+#ifndef RELATIONMANAGER_H_INCLUDED
+#define RELATIONMANAGER_H_INCLUDED
+#include <ctime>
+#include <QString>
+
+
+class RelationManager
+{
+    Relation** relations;
+    unsigned int nbRelations;
+    unsigned int nbMaxRelations;
+    void addRelation(Relation* r);
+    
+    
+    RelationManager();
+    ~RelationManager();
+    RelationManager(const RelationManager& m);
+    RelationManager& operator=(const RelationManager& m);
+    
+    public :
+    class Iterator {
+            friend class RelationManager;
+            Relation** currentR;
+            unsigned int nbRemain;
+            Iterator(Relation** r, unsigned nb):currentR(r),nbRemain(nb){}
+        public:
+            Iterator():nbRemain(0),currentR(nullptr){}
+            bool isDone() const { return nbRemain==0; }
+            void next() {
+                if (isDone())
+                    throw NoteException("error, next on an iterator which is done");
+                nbRemain--;
+                currentR++;
+            }
+            Relation& current() const {
+                if (isDone())
+                    throw NoteException("error, indirection on an iterator which is done");
+                return **currentR;
+            }
+};
+    
+    Iterator getIterator() {
+            return Iterator(relations,nbRelations);
+        }
+};
+
+#endif // RELATIONMANAGER_H_INCLUDED

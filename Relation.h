e24b5795a5308f15adcb4d74370cc4fb5ba519c2
CapucinePr <capucine.prudhomme@gmail.com>
2017-05-02 22:11:33 +0200

    Update Relation.h

--- Relation.h
+++ Relation.h
@@ -16,18 +16,23 @@ private:
 class Relation{
     friend class RelationManager;
     Couple **tab;
+    int nb;
+    int max;
     QString titre;
     QString description;
     bool orientee;
     Relation():orientee(True){} //constructeur sans argument, orientee vrai par défaut
-    ~Relation();
-    void SeeRelation();
+    ~Relation() {delete[] tab;}
+    void SeeRelation(); //utiliser un const_iterator pour avoir un accès en lecture ?
     Relation(Relation& r);
+    Relation& operator=(Relation& r);
     
     public :
-    QString getTitre(){return titre;}
-    QString getDesc(){return description;}
-    bool getOrient(){return orientee;}
+    QString getTitre() const {return titre;}
+    QString getDesc() const {return description;}
+    bool getOrient()const {return orientee;}
+    void addCouple(const Couple& c);
+        
 };
 
 #endif // RELATION_H_INCLUDED

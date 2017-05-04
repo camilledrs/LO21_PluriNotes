b4074f20bacab5431180d4eea987bc33bcf9de6e
CapucinePr <capucine.prudhomme@gmail.com>
2017-05-04 17:36:35 +0200

    Update Notes.h

--- Notes.h
+++ Notes.h
@@ -20,6 +20,7 @@ class Note //Class car on veut tout en privé
     tm dateCrea; //normalement tm est un type struct de la bibli ctime
     bool active;
     bool supprime;
+    Version **versions;  //faire un tableau ou une pile(FILO) serait encore mieux
     
     Note();
     ~Note();
@@ -33,6 +34,11 @@ class Note //Class car on veut tout en privé
     bool getActive(){return active;}
     bool getStatutSupp(){return supprime;}
     suppNote();  //à définir dans LO21.cpp  //attention ne supprimer que les archivées + voir autres conditions dans Drive
+    
+    //Je pense qu'il faut faire comme un VersionManager mais dans Note, en gros implémenter les fonctiosn nécessaires à la gestion du tableau/de la pile
+    Version* getDerniereVersion(); //faire dépiler en gros, ou alors prendre le dernier élément du tableau si on considère que l'on ajoute tjrs à la fin du tableau tableau 
 };
 
 #endif //NOTES_H_INCLUDED
+
+//avec getDerniereVersion dans Note, on pourrait ainsi choisir les dernières versions pour créer les couples, puisque c'est une contrainte imposée

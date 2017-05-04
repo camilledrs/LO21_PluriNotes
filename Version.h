7d4f450d7741423cad16d72fd5220f2212a3e314
CapucinePr <capucine.prudhomme@gmail.com>
2017-05-04 18:00:29 +0200

    Update Version.h

--- Version.h
+++ Version.h
@@ -17,3 +17,55 @@ class Version
         datetime getDate(){return date;}
 
 };
+
+
+class Article : public Version
+{
+        QString texte;
+        
+    public :
+        Article(const QString& s):texte(s){}
+        void editer();  //à voir, parce que ça veut dire créer une nouvelle version, on est obligés de la définir pour que ce ne soit plus une classe abstraite
+
+};
+
+enum Statut{'En attente', 'En cours', 'Terminee'};
+
+class Tache : public Version
+{
+        QString action;
+        Statut statut;
+        datetime dateTache;
+        unsigned int priorite;
+        
+     public :
+        Tachee(const QString a, datetime d=0, unsigned int p=0) :action(a), statut('En attente'), dateTache(d), priorite(p){}
+        editer(); //a voir
+        QString getAction(){return action;}
+        Statut getStatut(){return statut;}
+        unsigned int getPriority(){return priorite;}
+        datetime getDate() {return dateTache;}
+
+};
+
+
+enum Media {'image', 'audio', 'video'};
+
+
+class Multimedia : public Version
+{
+        QString description;
+        QString fichier;
+        Media type;
+        
+     public :
+        Multimedia(const QString& d, const QString& f, Media t):description(d), fichier(f), type(t){}
+        editer(); //a voir
+        QString getDescription() {return description;}
+        QString getFichier() {return fichier;}
+        Media getType() {return type;}
+
+};
+
+
+

7e52892ac3bc8d8f99d1363cac1eaa7d7208e4ae
CapucinePr <capucine.prudhomme@gmail.com>
2017-05-11 15:39:36 +0200

    Update couple.h

--- couple.h
+++ couple.h
@@ -12,9 +12,7 @@ class Couple //Class car on veut tout en privé
     int label
     Note* note1;
     Note* note2;
- 
-    Editer(){};
-    
+    Couple(const Couple& n);  //constructeur par recopie
     
     
     public  :            //si on les veut privées alors ça ne sert à rien, à discuter
@@ -23,7 +21,7 @@ class Couple //Class car on veut tout en privé
     setLabel(int newl){label=newl;}
     Couple(const Note& n1, const Note& n2, int l):label(l), note1(&n1), note2(&n2) {}
     ~Couple(){delete note1, delete note2;}
-    Couple(const Couple& n);  //constructeur par recopie
+    
      
 };
 

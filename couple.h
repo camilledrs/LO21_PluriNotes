e61557f66a4ba5a41a373205648c1c6c5cf0729e
CapucinePr <capucine.prudhomme@gmail.com>
2017-05-11 15:38:00 +0200

    Update couple.h

--- couple.h
+++ couple.h
@@ -20,8 +20,7 @@ class Couple //Class car on veut tout en privé
     public  :            //si on les veut privées alors ça ne sert à rien, à discuter
     
     int getLabel(){return label;}
-    //Couple(const Note& n1, const Note& n2, int l):label(l), note1(new Note(&n1)), note2(new Note(&n2)) {} //pourquoi new ? et pas &n1
-//en plus le constructeur par recopie de note est en prive !!
+    setLabel(int newl){label=newl;}
     Couple(const Note& n1, const Note& n2, int l):label(l), note1(&n1), note2(&n2) {}
     ~Couple(){delete note1, delete note2;}
     Couple(const Couple& n);  //constructeur par recopie

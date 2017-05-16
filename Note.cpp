c4aa0310a034a0e20e52aaed4017442db4b5985e
lucieWartelle <lucie.wartelle@laposte.net>
2017-05-16 10:23:34 +0200

    Update Note.cpp

--- Note.cpp
+++ Note.cpp
@@ -14,3 +14,57 @@ void Note::restaurer(Version* v)
     }
     versions[i]=tmp; //on met la version à restaurer au niveau de la dernière case du tableau (versions[nbVersion-1]) 
 }
+
+void editer(QString tittle, tm modif){
+	if (active && !supprime){
+		titre=tittle;
+		dateModif= modif;
+	}
+}
+
+void creerArticle(tm crea, const QString& s){
+	if (active && !supprime){
+		if (nbVersion==nbMaxVersion){
+			Version ** newVersions= new Version*[nbMaxVersions+5];
+			for(unsigned int i=0; i<nbVersion; i++)
+				newVersions[i]=versions[i];
+			Version** oldVersions=versions;
+			versions=newVersions;
+			nbMaxVerions+=5;
+			delete[] oldVersions;
+		}
+		versions[nbVersion++]= new Article(crea, s);
+	}
+}
+
+
+void creerMultimedia(tm crea, const QString& desc, const QString& file, Media t){
+	if (active && !supprime){
+		if (nbVersion==nbMaxVersion){
+			Version ** newVersions= new Version*[nbMaxVersions+5];
+			for(unsigned int i=0; i<nbVersion; i++)
+				newVersions[i]=versions[i];
+			Version** oldVersions=versions;
+			versions=newVersions;
+			nbMaxVerions+=5;
+			delete[] oldVersions;
+		}
+		versions[nbVersion++]= new Multimedia(crea, desc, file, t);
+	}
+}
+
+
+void creerTache(tm crea, const QString& a, tm dateFin=0, unsigned int prio=0){
+	if (active && !supprime){
+		if (nbVersion==nbMaxVersion){
+			Version ** newVersions= new Version*[nbMaxVersions+5];
+			for(unsigned int i=0; i<nbVersion; i++)
+				newVersions[i]=versions[i];
+			Version** oldVersions=versions;
+			versions=newVersions;
+			nbMaxVerions+=5;
+			delete[] oldVersions;
+		}
+		versions[nbVersion++]= new Tache(crea, a, dateFin, pris);
+	}
+}

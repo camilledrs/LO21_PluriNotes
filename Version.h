ca9e4c2449be61b250f09c51ed0714a7ba8c4238
Camille <camille.dresse@gmail.com>
2017-05-18 14:49:55 +0200

    Update Version.h

--- Version.h
+++ Version.h
@@ -10,10 +10,10 @@ class Version
         datetime date;
         friend class Note;
     
-        Version(Version&);
+        Version& Version(const Version&);
         virtual ~Version()=0;
         Version(datetime d):Date(d);
-        virtual Version$ clone()const=0;
+        virtual Version* clone()const=0;
         
         // j'ai mis ses méthodes en privée pour qu'elles ne puissent être utilisée que par les classes friend
     
@@ -26,7 +26,7 @@ class Version
 class Article : public Version
 {
         QString texte;
-        
+        Article& Article (const Article& );
         Article(datetime d,const QString& s):Version(d),texte(s){} // en privé pour qu'un article ne puisse être construit que par une classe friend
         Article* clone()const{return new Article(*this);}
     public :
@@ -42,7 +42,7 @@ class Tache : public Version
         Statut statut;
         datetime dateTache;
         unsigned int priorite;
-        
+        Tache& Tache(const Tache& );
         Tache(datetime dv,const QString a, datetime d=0, unsigned int p=0) :Version(dv),action(a), statut('En attente'), dateTache(d), priorite(p){} // en privé pour qu'une tache ne puisse être construite que par une classe friend
         Tache* clone()const{return new Tache(*this);}
      public :
@@ -64,7 +64,7 @@ class Multimedia : public Version
         QString description;
         QString fichier;
         Media type;
-        
+        Multimedia& Multimedia(const Multimedia& );
         Multimedia(datetime dv,const QString& d, const QString& f, Media t):Version(dv),description(d), fichier(f), type(t){} // en privé pour qu'un multimedia ne puisse être construit que par une classe friend
         Multimedia* clone()const{return new Multimedia(*this);} 
      public :

597d4ad46dfe003f7a01245428ef0781c44b3359
Camille <camille.dresse@gmail.com>
2017-05-18 12:20:29 +0200

    Update Version.h

--- Version.h
+++ Version.h
@@ -11,9 +11,9 @@ class Version
         friend class Note;
     
         Version(Version&);
-        virtual ~Version();
+        virtual ~Version()=0;
         Version(datetime d):Date(d);
-        virtual void editer()=0;   //class abstraite
+        //virtual void editer()=0;   //class abstraite
         // j'ai mis ses méthodes en privée pour qu'elles ne puissent être utilisée que par les classes friend
     
     public :
@@ -29,7 +29,7 @@ class Article : public Version
         Article(datetime d,const QString& s):Version(d),texte(s){} // en privé pour qu'un article ne puisse être construit que par une classe friend
         
     public :
-        void editer();  //à voir, parce que ça veut dire créer une nouvelle version, on est obligés de la définir pour que ce ne soit plus une classe abstraite
+        //void editer();  //à voir, parce que ça veut dire créer une nouvelle version, on est obligés de la définir pour que ce ne soit plus une classe abstraite
         // pour moi on n'a pas besoin d'une méthode éditer vu que l'on crée une nouvelle version à chaque édition, il faudra utiliser le constructeur
 };
 
@@ -45,7 +45,7 @@ class Tache : public Version
         Tache(datetime dv,const QString a, datetime d=0, unsigned int p=0) :Version(dv),action(a), statut('En attente'), dateTache(d), priorite(p){} // en privé pour qu'une tache ne puisse être construite que par une classe friend
         
      public :
-        editer(); //a voir // pour moi on n'a pas besoin d'une méthode éditer vu que l'on crée une nouvelle version à chaque édition, il faudra utiliser le constructeur
+        //editer(); //a voir // pour moi on n'a pas besoin d'une méthode éditer vu que l'on crée une nouvelle version à chaque édition, il faudra utiliser le constructeur
         QString getAction(){return action;}
         Statut getStatut(){return statut;}
         unsigned int getPriority(){return priorite;}
@@ -67,7 +67,7 @@ class Multimedia : public Version
      
      public :
         
-        editer(); //a voir // pour moi on n'a pas besoin d'une méthode éditer vu que l'on crée une nouvelle version à chaque édition, il faudra utiliser le constructeur
+        //editer(); //a voir // pour moi on n'a pas besoin d'une méthode éditer vu que l'on crée une nouvelle version à chaque édition, il faudra utiliser le constructeur
         QString getDescription() {return description;}
         QString getFichier() {return fichier;}
         Media getType() {return type;}

c2be4b6f37bf2c5a495c9a1a0869179999563d24
CapucinePr <capucine.prudhomme@gmail.com>
2017-05-24 17:25:43 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,31 +2,34 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
-    zoneCentrale = new QWidget;
-    zoneGauche = new QDockWidget;
+    QWidget* zoneCentrale = new QWidget;
+    QDockWidget* zoneGauche = new QDockWidget;
     zoneGauche->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
     //zoneGauche->setWidget();
     addDockWidget(Qt::LeftDockWidgetArea, zoneGauche);
 
-    zoneDroite = new QDockWidget;
+    QDockWidget* zoneDroite = new QDockWidget;
     zoneDroite->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
     //zoneDroite->setWidget();
     addDockWidget(Qt::RightDockWidgetArea, zoneDroite);
 
-    idNote = new QLineEdit;
-    boutonAfficher = new QPushButton("Afficher une note");
+    QLineEdit* idNote = new QLineEdit;
+    QPushButton* boutonAfficher = new QPushButton("Afficher une note");
 
-    layout = new QFormLayout;
+
+    QFormLayout* layout = new QFormLayout;
     layout->addRow("Id", idNote);
 
-    layoutRecherche = new QVBoxLayout;
+    
+    QVBoxLayout* layoutRecherche = new QVBoxLayout;
     layoutRecherche->addLayout(layout);
     layoutRecherche->addWidget(boutonAfficher);
 
     QObject::connect(boutonAfficher,SIGNAL(clicked()),this,SLOT(Recherche()));
 
-    layoutPrincipal = new QVBoxLayout;
+    QVBoxLayout* layoutPrincipal = new QVBoxLayout;
     layoutPrincipal->addLayout(layoutRecherche);
+
 
     //layoutGauche = new QHBoxLayout;
 
@@ -35,6 +38,48 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 
     setCentralWidget(zoneCentrale);
 }
+
+
+/*
+    
+    QWidget* Relation = new QWidget;
+    QWidget* zoneCentraleRel = new QWidget(&Relation);
+    QDockWidget* zoneGauche = new QDockWidget;
+    
+    QLineEdit* Titre = new QLineEdit;
+    QLineEdit* Desc = new QLineEdit;
+    
+    QFormLayout* layoutRel = new QFormLayout;
+    layoutRel->addRow("Titre :", Titre);
+    layoutRel->addRow("Description :", Desc);
+    QPushButton* boutonAfficherRel = new QPushButton("Afficher une Relation");
+    QObject::connect(boutonAfficherRel,SIGNAL(clicked()),this,SLOT(Relation::SeeRelation()));
+    layoutRel->addWidget(boutonAfficherRel);
+    
+    
+    QPushButton* boutonCreer = new QPushButton("Creer une note");
+    QPushButton* boutonEditer = new QPushButton("Editer une note");
+    QPushButton* boutonSupprimer = new QPushButton("Supprimer une note");
+    QPushButton* boutonEnrichir = new QPushButton("Enrichir une note");
+    QHBox* layoutBouttons= new QHBoxLayout;
+    layoutBouttons->addWidget(boutonCreer);
+    QObject::connect(boutonCreer,SIGNAL(clicked()),this,SLOT(Relation::Relation(.....))); //mettre des parametres, les récuperer des champs titre et description
+    layoutBouttons->addWidget(boutonEditer);
+    QObject::connect(boutonEditer,SIGNAL(clicked()),this,SLOT(Relation::Editer()));
+    layoutBouttons->addWidget(boutonSupprimer);
+    QObject::connect(boutonSupprimer,SIGNAL(clicked()),this,SLOT(Relation::~Relation()));
+    layoutBouttons->addWidget(boutonEnrichir);
+    QObject::connect(boutonEnrichir,SIGNAL(clicked()),this,SLOT(ajouterCouple()));
+    //slot à creer, faire choisir 2 notes et un label et faire appel à addcouple()
+    
+    QVBoxLayout* layoutPrincipalRel = new QVBoxLayout;
+    layoutPrincipalRel->addLayout(layoutRel);
+    layoutPrincipalRel->addLayouy(layoutBouttons);
+    
+    zoneCentraleRel->setLayout(layoutPrincipalRel)
+    
+*/
+
 
 void MainWindow::Recherche(){
 
@@ -51,7 +96,7 @@ void MainWindow::Recherche(){
     //contenuNote->setText(note->getContenu); //à voir
 
 //    QLabel *titre= new QLabel("Note");
-    enregistrer = new QPushButton("Enregistrer");
+    QPushButton* enregistrer = new QPushButton("Enregistrer");
 
     layoutAffich = new QFormLayout;
     layoutAffich->addRow("Titre", titreNote);

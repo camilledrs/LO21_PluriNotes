a9cfc0a2948357ea2935b2e3e6095e4b1f7d57a1
CapucinePr <capucine.prudhomme@gmail.com>
2017-05-25 12:28:56 +0200

    Update WindowRelation.cpp

--- WindowRelation.cpp
+++ WindowRelation.cpp
@@ -1,4 +1,5 @@
 #include "WindowRelation.h"
+#include "mainwindow.h"
 
 WindowRelation::WindowRelation(QWidget *parent) : MainWindow(parent)  //pour dire que c'est la fenetre principale MainWindow le parent de notre fenetre
 //pas sure du tout
@@ -28,9 +29,9 @@ WindowRelation::WindowRelation(QWidget *parent) : MainWindow(parent)  //pour dir
     layoutBouttons->addWidget(boutonCreer);
     QObject::connect(boutonCreer,SIGNAL(clicked()),this,SLOT(Creer())); //mettre des parametres, les récuperer des champs titre et description
     layoutBouttons->addWidget(boutonEditer);
-    QObject::connect(boutonEditer,SIGNAL(clicked()),this,SLOT(Editer()));  //Creer le slot avec Relation::creer
+    QObject::connect(boutonEditer,SIGNAL(clicked()),this,SLOT(Editer()));  
     layoutBouttons->addWidget(boutonSupprimer);
-    QObject::connect(boutonSupprimer,SIGNAL(clicked()),this,SLOT(Relation::~Relation()));
+    QObject::connect(boutonSupprimer,SIGNAL(clicked()),this,SLOT(Supprimer()));
     layoutBouttons->addWidget(boutonEnrichir);
     QObject::connect(boutonEnrichir,SIGNAL(clicked()),this,SLOT(ajouterCouple()));
     //slot à creer, faire choisir 2 notes et un label et faire appel à addcouple()
@@ -44,3 +45,34 @@ WindowRelation::WindowRelation(QWidget *parent) : MainWindow(parent)  //pour dir
 
 
 }
+
+
+
+
+void WindowRelation::Creer()
+{
+    
+    QString titre = new QInputDialog::getText(this, "Titre :", "Quel titre voulez vous ?", QLineEdit::Normal, QString(), &ok1);
+    QString desc = new QInputDialog::getText(this, "Titre :", "Quel titre voulez vous ?", QLineEdit::Normal, QString(), &ok2);
+    			if (ok1 && ok2 && !titre.isEmpty() && !desc.isEmpty())
+    				{
+       					 Relation::Relation(titre, desc);
+                         QMessageBox::information(this, "Confirmation creation", "La nouvelle relation a bien été créée ! ");
+                    }
+
+}
+
+void WindowRelation::ajouterCouple()
+{
+    //QString id1 = new QInputDialog::getText(this, "ID Note1 :", "Entrez l'id de la premiere note à mettre dans le couple", QLineEdit::Normal, QString(), &ok1);
+    //while ((iterator!=it_end) || /*((n.getId() != iterator.courant->getIdNote1) && */(n.getId() != iterator.courant->getIdNote2())//)
+	//	iterator++;
+    //if (iterator!=it_end)
+    //QString id2 = new QInputDialog::getText(this, "ID Note2 :", "Entrez l'id de la deuxième note à mettre dans le couple", QLineEdit::Normal, QString(), &ok2);
+    
+    
+}
+
+
+
+

92cfdcc718defff6f79597d75d47dcb803060cc0
CapucinePr <capucine.prudhomme@gmail.com>
2017-05-25 11:49:13 +0200

    Create WindowRelation.h

---
+++ WindowRelation.h
@@ -0,0 +1,35 @@
+#ifndef WINDOWRELATION_H
+#define WINDOWRELATION_H
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QWidget>
+#include <QLineEdit>
+#include <QFormLayout>
+#include <QLayout>
+#include <QLabel>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QDockWidget>
+#include <QTabWidget> //pour les onglet
+
+
+
+class WindowRelation : public QTabWidget
+{
+        Q_OBJECT
+ 
+    public:
+        WindowRelation(QWidget *parent);
+ 
+    public slots:
+    
+ 
+    private:
+        //(...)
+ 
+ 
+};
+
+
+#endif

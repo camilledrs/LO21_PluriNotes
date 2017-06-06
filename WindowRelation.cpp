069dc4a017476aa2e35fb114a1b397aea5c8eff4
CapucinePr <capucine.prudhomme@gmail.com>
2017-06-06 14:29:40 +0200

    Update WindowRelation.cpp

--- WindowRelation.cpp
+++ WindowRelation.cpp
@@ -1,49 +1,43 @@
-#include "WindowRelation.h"
-#include "mainwindow.h"
+#include "windowrelation.h"
+//#include "mainwindow.h"
 
-WindowRelation::WindowRelation(QWidget *parent) : MainWindow(parent)  //pour dire que c'est la fenetre principale MainWindow le parent de notre fenetre
-//pas sure du tout
-{
-    QWidget* zoneCentraleRel = new QWidget;  
+WindowRelation::WindowRelation(QWidget *parent) : QMainWindow(parent) { //QTabWidget(parent)
+    QWidget* zoneCentraleRel = new QWidget;
     QDockWidget* zoneGaucheRel = new QDockWidget;
     zoneGaucheRel->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
     //zoneGaucheRel->setWidget();
     addDockWidget(Qt::LeftDockWidgetArea, zoneGaucheRel);
-    
+
     QLineEdit* Titre = new QLineEdit;
     QLineEdit* Desc = new QLineEdit;
-    
+
     QFormLayout* layoutRel = new QFormLayout;
     layoutRel->addRow("Titre :", Titre);
     layoutRel->addRow("Description :", Desc);
     QPushButton* boutonAfficherRel = new QPushButton("Afficher une Relation");
     QObject::connect(boutonAfficherRel,SIGNAL(clicked()),this,SLOT(SeeRelation()));
     layoutRel->addWidget(boutonAfficherRel);
-    
-    
+
+
     QPushButton* boutonCreer = new QPushButton("Creer une note");
     QPushButton* boutonEditer = new QPushButton("Editer une note");
     QPushButton* boutonSupprimer = new QPushButton("Supprimer une note");
     QPushButton* boutonEnrichir = new QPushButton("Enrichir une note");
-    QHBox* layoutBouttons= new QHBoxLayout;
+    QHBoxLayout* layoutBouttons= new QHBoxLayout;
     layoutBouttons->addWidget(boutonCreer);
     QObject::connect(boutonCreer,SIGNAL(clicked()),this,SLOT(Creer())); //mettre des parametres, les récuperer des champs titre et description
     layoutBouttons->addWidget(boutonEditer);
-    QObject::connect(boutonEditer,SIGNAL(clicked()),this,SLOT(Editer()));  
+    QObject::connect(boutonEditer,SIGNAL(clicked()),this,SLOT(Editer()));
     layoutBouttons->addWidget(boutonSupprimer);
     QObject::connect(boutonSupprimer,SIGNAL(clicked()),this,SLOT(Supprimer()));
     layoutBouttons->addWidget(boutonEnrichir);
     QObject::connect(boutonEnrichir,SIGNAL(clicked()),this,SLOT(ajouterCouple()));
-    //slot à creer, faire choisir 2 notes et un label et faire appel à addcouple()
-    
+
     QVBoxLayout* layoutPrincipalRel = new QVBoxLayout;
     layoutPrincipalRel->addLayout(layoutRel);
-    layoutPrincipalRel->addLayouy(layoutBouttons);
-    
-    zoneCentraleRel->setLayout(layoutPrincipalRel)
-    
+    layoutPrincipalRel->addLayout(layoutBouttons);
 
-
+    zoneCentraleRel->setLayout(layoutPrincipalRel);
 }
 
 
@@ -51,57 +45,57 @@ WindowRelation::WindowRelation(QWidget *parent) : MainWindow(parent)  //pour dir
 
 void WindowRelation::Creer()
 {
-    
-    QString titre = new QInputDialog::getText(this, "Titre :", "Quel titre voulez vous ?", QLineEdit::Normal, QString(), &ok1);
-    QString desc = new QInputDialog::getText(this, "Titre :", "Quel titre voulez vous ?", QLineEdit::Normal, QString(), &ok2);
-    			if (ok1 && ok2 && !titre.isEmpty() && !desc.isEmpty())
-    				{
-       					 Relation::Relation(titre, desc);
-                         QMessageBox::information(this, "Confirmation creation", "La nouvelle relation a bien été créée ! ");
-                    }
-
-}
-
-
-
-void WindowRelation::ajouterCouple()
-{
-    	bool ok=False;
-	while(!ok){
-	NoteManager::Iterator it=getIterator();
-	while(!it.isDone()){ //parcours les notes
-    QString id1 = new QInputDialog::getText(this, "ID Note1 :", "Entrez l'id de la premiere note à mettre dans le couple", QLineEdit::Normal, QString(), &ok1);
-    while ((!it.isDone()) && (id1 != it.currentN->getId()))
-		it++;
-    if (!it.isDone())
+    bool ok1=false;
+    bool ok2=false;
+    QString titre = QInputDialog::getText(this, "Titre :", "Quel titre voulez vous ?", QLineEdit::Normal, QString(), &ok1);
+    QString desc = QInputDialog::getText(this, "Titre :", "Quel titre voulez vous ?", QLineEdit::Normal, QString(), &ok2);
+    if (ok1 && ok2 && !titre.isEmpty() && !desc.isEmpty())
     {
-	Note* n1=iterator.currentN;
-	 ok=True;
-    else 
-	    QMessageBox::critical(this, "Note non existante", "L'id ne correspond à aucune note, ressaisir un nouvel Id");
-    }	
-   ok=False;
-	while(!ok){
-	NoteManager::Iterator it=getIterator();
-	while(!it.isDone()){ //parcours les notes
-    QString id2 = new QInputDialog::getText(this, "ID Note2 :", "Entrez l'id de la seconde note à mettre dans le couple", QLineEdit::Normal, QString(), &ok1);
-    while ((!it.isDone()) && (id2 != it.currentN->getId()))
-		it++;
-    if (!it.isDone())
-    {
-	Note* n2=iterator.currentN;
-	 ok=True;
-    else 
-	    QMessageBox::critical(this, "Note non existante", "L'id ne correspond à aucune note, ressaisir un nouvel Id");
+        Relation *rel=new Relation(titre, desc);
+        RelationManager::getInstance().addRelation(*rel);
+        //RelationManager::addRelation(titre, desc);
+        QMessageBox::information(this, "Confirmation creation", "La nouvelle relation a bien été créée ! ");
     }
-   QString strl = new QInputDialog::getText(this, "Couple miroir", "Quellabel voulez vous pour le couple miroir ?", QLineEdit::Normal, QString(), &ok);	
-    if (ok && !strl.isEmpty())
-    				{
-       					 int l=atoi(strl);
-				}
-    RelationManager::getInstance()->addCouple(*n1, *n2,l);	
 }
 
 
 
-
+void WindowRelation::ajouterCouple(Relation *r)
+{
+    bool ok=false;
+    Note* n1;
+    Note* n2;
+    //while(!ok)
+        QString id1 = QInputDialog::getText(this, "ID Note1 :", "Entrez l'id de la premiere note à mettre dans le couple", QLineEdit::Normal, QString(), &ok);
+    NoteManager::Iterator it=NoteManager::getInstance().getIterator();
+    while ((!it.isDone()) && (id1 != it.current().getId())) //parcours les notes
+        it.next();
+    if (!it.isDone())
+    {
+        n1=(&it.current());
+        ok=true;
+    }
+    else
+        QMessageBox::critical(this, "Note non existante", "L'id ne correspond à aucune note, ressaisir un nouvel Id");
+    ok=false;
+    //while(!ok)
+        QString id2 = QInputDialog::getText(this, "ID Note2 :", "Entrez l'id de la seconde note à mettre dans le couple", QLineEdit::Normal, QString(), &ok);
+    NoteManager::Iterator itN=NoteManager::getInstance().getIterator();
+    while ((!itN.isDone()) && (id2 != itN.current().getId())) //parcours les notes
+        itN.next();
+    if (!itN.isDone())
+    {
+        n2=&itN.current();
+        ok=true;
+    }
+    else
+        QMessageBox::critical(this, "Note non existante", "L'id ne correspond à aucune note, ressaisir un nouvel Id");
+    if (n2 && n1){
+    QString strl = QInputDialog::getText(this, "Couple miroir", "Quel label voulez vous pour le couple miroir ?", QLineEdit::Normal, QString(), &ok);
+    if (ok && !strl.isEmpty()){
+        const char* c= strl.toStdString().c_str(); //pour convertir QString en const char* et ensuite pouvoir appeler atoi
+        int l=atoi(c);
+        r->addCouple(*n1,*n2,l);
+    }
+    }
+}

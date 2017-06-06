c2c67f0c1ba7463fc191b225f724f0a71f9daf22
CapucinePr <capucine.prudhomme@gmail.com>
2017-06-06 14:30:31 +0200

    Update couple.h

--- couple.h
+++ couple.h
@@ -1,33 +1,50 @@
-#ifndef COUPLE_H_INCLUDED
-#define COUPLE_H_INCLUDED
+#ifndef COUPLE_H
+#define COUPLE_H
+
 #include <ctime>
 #include <QString>
-#include "Notes.h"
+#include "note.h"
 
+
+/*! Classe Couple*/
+
+//! La classe Couple regroupe deux pointeurs de Note ainsi qu'un label
 
 
 class Couple //Class car on veut tout en privé
 {
-    
-    int label;
-    Note* note1;
-    Note* note2;
-    Couple(const Couple& n);  //constructeur par recopie
-    
-    
-    public  :            //si on les veut privées alors ça ne sert à rien, à discuter
-    
+
+    int label; /*< Attribut label entier, unique */
+    Note* note1;  /*< Pointeur sur un objet de classe Note, premier élément du couple*/
+    Note* note2;  /*< Pointeur sur un objet de classe Note, deuxième élément du couple*/
+    Couple(const Couple& n);  //! constructeur par recopie
+
+    public  :
+    //! acceseur pour permettre d'obtenir le label
+    //! \return le label du Couple
     int getLabel(){return label;}
+    //! accesseur pour la note 1
+    //! \return un poiteur sur la note 1
     Note* getNote1(){return note1;}
+    //! accesseur pour la note 2
+    //! \return un poiteur sur la note 2
     Note* getNote2(){return note2;}
-    QString getIdNote1(){return note1->getId();}
-    QString getIdNote2(){return note2->getId();}
+    //! acceseur pour permettre d'obtenir l'ID de la note 1
+    //! \return l(e label'ID de la première note du couple
+    QString getIdNote1()const {return note1->getId();}
+    //! acceseur pour permettre d'obtenir l'ID de la note 2
+    //! \return l(e label'ID de la deuxième note du couple
+    QString getIdNote2() const {return note2->getId();}
+    //! methode pour modifier le label du couple
+    //! \param newl un entier correspondant au nouveau label
     void setLabel(int newl){label=newl;}
-    Couple(const Note& n1, const Note& n2, int l):label(l), note1(&n1), note2(&n2) {}
-    ~Couple();
-    //~Couple(){delete note1, delete note2;} // ça veut dire que l'on détruit les notes lorsque l'on détruit le couple ?)
-    
-     
+    //! Un constructeur avec 3 arguments
+    //! \param n1 une référence sur une note, que l'on veut en note 1
+    //! \param n2 une reference sur une note que l'on veut en note 2
+    //! \param l l'entier correspondant au label voulu
+    Couple(Note& n1, Note& n2, int l):label(l), note1(&n1), note2(&n2) {}
+   //! un destructeur, qui libère l'espace mémoire alloué aux deux pointeurs de note
+    ~Couple(){}
 };
 
-#endif //COUPLE_H_INCLUDED
+#endif // COUPLE_H

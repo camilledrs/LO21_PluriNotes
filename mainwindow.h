09f26068aef20bc827ee662b5398e8534c6a13f5
CapucinePr <capucine.prudhomme@gmail.com>
2017-06-06 14:32:05 +0200

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -10,17 +10,12 @@
 #include <QLabel>
 #include <QTextEdit>
 #include <QPushButton>
-#include <QDockWidget>
-#include <QTabWidget>  //pour les onglet
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
     QWidget *zoneCentrale;
-    QDockWidget *zoneGauche;
-    QDockWidget *zoneDroite;
-
     QLineEdit *titreNote;
     QLineEdit *dateCreaNote;
     QTextEdit *contenuNote;
@@ -35,7 +30,6 @@ class MainWindow : public QMainWindow
     QVBoxLayout *layoutRecherche;
     QVBoxLayout *layoutPrincipal;
 
-
 public:
     explicit MainWindow(QWidget *parent = 0);
 
@@ -43,9 +37,6 @@ signals:
 
 public slots:
     void Recherche();
-    void nouvelleFen();
-    void viderLaCorbeille(){NoteManager::getInstance->viderCorbeille();}
-    void quitter();
 };
 
 #endif // MAINWINDOW_H

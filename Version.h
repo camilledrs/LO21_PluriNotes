5d6e36fd023c54dd9162061d028b6d6e5ef93cc2
lucieWartelle <lucie.wartelle@laposte.net>
2017-06-06 10:42:57 +0200

    Update Version.h

--- Version.h
+++ Version.h
@@ -7,19 +7,19 @@
 
 class Version
 {
-        datetime date;
+        tm date;
         friend class Note;
     
        Version(const Version&);
         virtual ~Version()=0;
-        Version(datetime d):Date(d);
+        Version(tm d):Date(d);
         virtual Version* clone()const=0;
         
         // j'ai mis ses méthodes en privée pour qu'elles ne puissent être utilisée que par les classes friend
     
     public :
         
-        datetime getDate(){return date;}
+        tm getDate(){return date;}
 };
 
 
@@ -27,7 +27,7 @@ class Article : public Version
 {
         QString texte;
         Article (const Article& );
-        Article(datetime d,const QString& s):Version(d),texte(s){} // en privé pour qu'un article ne puisse être construit que par une classe friend
+        Article(tm d,const QString& s):Version(d),texte(s){} // en privé pour qu'un article ne puisse être construit que par une classe friend
         Article* clone()const{return new Article(*this);}
         ~Article();
     public :
@@ -41,19 +41,19 @@ class Tache : public Version
 {
         QString action;
         Statut statut;
-        datetime dateTache;
+        tm dateTache;
         unsigned int priorite;
         
         ~Tache();
         Tache(const Tache& );
-        Tache(datetime dv,const QString a, datetime d=0, unsigned int p=0) :Version(dv),action(a), statut('En attente'), dateTache(d), priorite(p){} // en privé pour qu'une tache ne puisse être construite que par une classe friend
+        Tache(tm dv,const QString a, tm d=0, unsigned int p=0) :Version(dv),action(a), statut('En attente'), dateTache(d), priorite(p){} // en privé pour qu'une tache ne puisse être construite que par une classe friend
         Tache* clone()const{return new Tache(*this);}
      public :
         
         QString getAction(){return action;}
         Statut getStatut(){return statut;}
         unsigned int getPriority(){return priorite;}
-        datetime getDate() {return dateTache;}
+        tm getDate() {return dateTache;}
         
 
 };
@@ -70,7 +70,7 @@ class Multimedia : public Version
         
         ~Multimedia();
         Multimedia(const Multimedia& );
-        Multimedia(datetime dv,const QString& d, const QString& f, Media t):Version(dv),description(d), fichier(f), type(t){} // en privé pour qu'un multimedia ne puisse être construit que par une classe friend
+        Multimedia(tm dv,const QString& d, const QString& f, Media t):Version(dv),description(d), fichier(f), type(t){} // en privé pour qu'un multimedia ne puisse être construit que par une classe friend
         Multimedia* clone()const{return new Multimedia(*this);} 
      public :
         

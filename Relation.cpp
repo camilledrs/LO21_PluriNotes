f251bf7188b7e3583343d459140c20d470146f79
CapucinePr <capucine.prudhomme@gmail.com>
2017-06-06 14:27:27 +0200

    Update Relation.cpp

--- Relation.cpp
+++ Relation.cpp
@@ -1,197 +1,182 @@
+#include "relation.h"
 #include <ctime>
 #include <QString>
 #include "Relation.h"
 #include "Couple.h"
 #include "NoteManager.h"
+#include <iostream>
+#include <QMessageBox>
 
 
-Relation::Relation(Relation& r):tab(new *Couple[r.max]),nb(r.nb), max(r.max), titre(r.titre), description(r.description), orientee(r.orientee){
-  for(unsigned int i=0; i<nb; i++) tab[i]=r.tab[i];
-  }
-  
-
-Relation::Relation& Relation::operator=(const Relation& r){
-  if(this!=&r)
-  {
-      nb=r.nb;
-      max=r.max;
-      titre=r.titre;
-      description=r.description;
-      orientee=r.orientee;
-      newtab=new Couple*[max];
-      for(unsigned int i=0; i<nb; i++) newtab[i]=r.tab[i];
-      delete[] tab;
-      tab=newtab;
-   }
-   return *this;
-}
-      
-
-
-void Relation::addCouple(const Note& n1, const Note& n2, int l){  //verifier ici qu'on veut la dernière version ?
-	if(!*this.getOrient()) //relation pas orientee, faire 2 couples
-	{
-		QString strl2 = new QInputDialog::getText(this, "Couple miroir", "Quellabel voulez vous pour le couple miroir ?", QLineEdit::Normal, QString(), &ok);	
-    		if (ok && !strl2.isEmpty())
-    				{
-       					 int l2=atoi(strl2);
-    				}
-		for(unsigned int i=0; i<nb; i++){
-		if (tab[i]->getLabel()==l2) throw NoteException("error, creation of an already existent note");
-		}
-		if (nb==max){
-			Couple** newtab= new Couple*[max+5];
-			for(unsigned int i=0; i<nb; i++) newtab[i]=couple[i];
-			Couple** old=couple;
-			couple=newtab;
-			max+=5;
-			if (old) delete[] old;
-		}
-		couple[nb++]= new Couple(n2, n1, l2);
-	}
-	for(unsigned int i=0; i<nb; i++){
-		if (tab[i]->getLabel()==l) throw NoteException("error, creation of an already existent note");
-		}
-	if (nb==max){
-		Couple** newtab= new Couple*[max+5];
-		for(unsigned int i=0; i<nb; i++) newtab[i]=couple[i];
-		Couple** old=couple;
-		couple=newtab;
-		max+=5;
-		if (old) delete[] old;
-		}
-		//couple[nb++]=c;//attention c'est une compo
-	couple[nb++]= new Couple(n1, n2, l);
-	if (this.orientee==false)
-		couple[nb++]= new Couple(n2, n1, l);
-	}
-}
-
-
-void Relation::suppCouple(const Couple& c)
+Relation::Relation(Relation& r):tab(new Couple*[r.max]),nb(r.nb), max(r.max), titre(r.titre), description(r.description), orientee(r.orientee)
 {
-	
-	unsigned int i=0;
-	while(i<nb && tab[i]->getLabel()!=c.getLabel())
-		i++;
-	if (i==nb) throw Note::NoteException("error, the item doesn't exist");
-
-	else 
-	{
-		Note* note1=c.getNote1();
-		Note* note2=c.getNote2();
-		if((note1->active==true) && (note2->active==true)){ //on ne peut supprimer que les couples ne contenant aucune note archivée
-		bool orient=c.getOrient();
-		if( orient== false) //besoin de supprimer aussi la relation "miroir" (y,x)
-		{
-			unsigned int j=0;
-			while(j<nb && ((tab[j]->getIdNote1() != note2->getId()) || (tab[j]->getIdNote2()!= note1->getId())))
-				j++;
-			if (j==nb) throw Note::NoteException("error, the mirror item doesn't exist\n");
-			else  //on supprime le couple miroir (y,x)
-			{
-				
-				int inij=j;
-				delete tab[j];
-				while(j<nb-1) 
-				{
-					tab[j]=tab[j+1];
-					j++;
-				}
-				tab[nb-1]=nullptr; //on a décalé, on met l'ancien dernier à NULL vu qu'on diminue la taille du tableau
-				nb--;
-				if (i>inij)  //on a décalé du coup, et le i initial doit être décrémenté de 1 si on veut accéder au bon couple
-				{
-					i--;
-				}
-			}
-		}
-		//maitenant on supprime le couple (x,y)
-		delete tab[i];
-		while(i<nb-1) 
-		{
-			tab[i]=tab[i+1];
-			i++;
-		}
-		tab[nb-1]=nullptr; //on a décalé, on met l'ancien dernier à NULL vu qu'on diminue la taille du tableau
-		nb--;
-		RelationManager& instance=RelationManager::getInstance();
-		Relation* ref=instance.getRef();
-		if (note2->getActive() == false){  //note archivee, on doit regardee si elle est note2 au moins une fois dans Reference
-			Relation::const_iterator it=ref->begin();
-			Relation::const_iterator end=ref->end();
-			while(it!=end && it.courant->note2 != note2)
-			{
-				it++;
-			}
-			if(it==end) //la note n'est plus en couple nulle part
-			{
-				int reponse= QMessageBox::question(fenetre,"Supprimer de note", "La note " note2->getId() " est archivée et n'est plus référencée, voulez-vous  la supprimer ?",QMessageBox::Yes | QMessageBox::No);
-				if(reponse == QMessageBox::Yes)
-					delete note2;
-		 		// fait apparaitre une fenêtre de dialogue avec l’utilisateur
-			}
-		}
-		if (!orient){ //on regarde aussi si note1 n'est plus référencée puisque on a supprimé le couple miroir
-		if (note1->getActive() == false){
-			Relation::const_iterator it=ref->begin();
-			Relation::const_iterator end=ref->end();
-			while(it!=end && it.courant->note2 != note1)
-			{
-				it++;
-			}
-			if(it==end) //la note n'est plus en couple nulle part
-			{
-				int reponse= new QMessageBox::question(???,"Supprimer de note", "La note " note1->getId() " est archivée et n'est plus référencée, voulez-vous  la supprimer ?", QMessageBox::Yes | QMessageBox::No);
-				if(reponse == QMessageBox::Yes)
-					delete note1;
-		 		// fait apparaitre une fenêtre de dialogue avec l’utilisateur
-			}
-		}
-		}
-				}
-		else  //une au moins des notes est archivées
-		throw Note::NoteException("error, one of the note is archived");
-	}
-
+    for(unsigned int i=0; i<nb; i++) tab[i]=r.tab[i];
 }
 
 
-	
-void Relation::seeRelation(){
-	std::cout<<"titre = "<<titre<<"\n"<<"description = "<<description<<"\n"<<"orientation = "<<orientee<<"\n";
-	Relation::const_iterator it=begin();
-	Relation::const_iterator end=end();
-	if(it!=end){
-	do
-	{
-		cout<<"label : "<<it.courant->getLabel()<<"\n"<<"note1 : "<<it.courant->getIdNote1()<<"\n"<<"note 2 : "<<it.courant->getIdNote2()<<"\n";);
-		it++;
-	} while(it!=end);
-	}
+Relation& Relation::operator=(Relation& r)
+{
+    if(this!=&r)
+    {
+        nb=r.nb;
+        max=r.max;
+        titre=r.titre;
+        description=r.description;
+        orientee=r.orientee;
+        Couple** newtab=new Couple*[max];
+        for(unsigned int i=0; i<nb; i++) newtab[i]=r.tab[i];
+        delete[] tab;
+        tab=newtab;
+    }
+    return *this;
 }
-	
-	
-	
-void Relation::editer(){
-	unsigned int rep;
-	QString titre, desc;
-	int reponse= new QMessageBox::question(???,"Editer titre", "Voulez vous éditer le titre ? ", QMessageBox::Yes | QMessageBox::No);
-	if(reponse == QMessageBox::Yes)
-	{
-		    QString titre = new QInputDialog::getText(this, "Nouveau titre", "Quel titre voulez vous ?", QLineEdit::Normal, QString(), &ok);	
-    			if (ok && !titre.isEmpty())
-    				{
-       					 setTitre(titre);
-    				}
-	}
-	int reponse=new QMessageBox::question(???,"Editer description", "Voulez vous éditer la description ?", QMessageBox::Yes | QMessageBox::No);
-	if(reponse == QMessageBox::Yes)
-	{
-		    QString desc = new QInputDialog::getText(this, "Nouvelle description", "Quel description voulez vous ?", QLineEdit::Normal, QString(), &ok);	
-    		    if (ok && !desc.isEmpty())
-    				{
-       					 setDesc(desc);
-    				}
-	}
+
+
+
+void Relation::addCouple(Note &n1, Note &n2, int l)  //verifier ici qu'on veut la dernière version ?
+{
+    if(!this->getOrient()) //relation pas orientee, faire 2 couples
+    {
+        int l2;
+        std::cout<<"quel nouveau label pour le couple miroir ?\n";
+        std::cin>>l2;
+        for(unsigned int i=0; i<nb; i++)
+            if (tab[i]->getLabel()==l2) throw NoteException("error, creation of an already existent note");
+        if (nb==max)
+        {
+            Couple** newtab= new Couple*[max+5];
+            for(unsigned int i=0; i<nb; i++) newtab[i]=this->tab[i];
+            Couple** old=this->tab;
+            this->tab=newtab;
+            max+=5;
+            if (old) delete[] old;
+        }
+        tab[nb++]= new Couple(n2, n1, l2);
+    }
+    for(unsigned int i=0; i<nb; i++)
+        if (tab[i]->getLabel()==l) throw NoteException("error, creation of an already existent note");
+    if (nb==max)
+    {
+        Couple** newtab= new Couple*[max+5];
+        for(unsigned int i=0; i<nb; i++) newtab[i]=tab[i];
+        Couple** old=tab;
+        tab=newtab;
+        max+=5;
+        if (old) delete[] old;
+    }
+    //couple[nb++]=c;//attention c'est une compo
+    tab[nb++]= new Couple(n1, n2, l);
+    if (this->getOrient()==false)
+        tab[nb++]= new Couple(n2, n1, l);
+}
+
+
+void Relation::suppCouple(Couple& c)
+{
+    unsigned int i=0;
+    while(i<nb && tab[i]->getLabel()!=c.getLabel())
+        i++;
+    if (i==nb)
+        throw NoteException("error, the item doesn't exist");
+    else
+    {
+        Note* note1=c.getNote1();
+        Note* note2=c.getNote2();
+        if(this->getOrient() == false) //besoin de supprimer aussi la relation "miroir" (y,x)
+        {
+            unsigned int j=0;
+            while(j<nb && ((tab[j]->getIdNote1() != note2->getId()) || (tab[j]->getIdNote2()!= note1->getId())))
+                j++;
+            int inij=j;
+            if (j==nb)
+                throw NoteException("error, the mirror item doesn't exist\n");
+            else  //on supprime le couple miroir (y,x)
+            {
+                delete tab[j];
+                while(j<nb-1)
+                {
+                    tab[j]=tab[j+1];
+                    j++;
+                }
+                tab[nb-1]=NULL; //on a décalé, on met l'ancien dernier à NULL vu qu'on diminue la taille du tableau
+                nb--;
+                if (i>inij)  //on a décalé du coup, et le i initial doit être décrémenté de 1 si on veut accéder au bon couple
+                {
+                    i--;
+                }
+            }
+        }
+        //maitenant on supprime le couple (x,y)
+        delete tab[i];
+        while(i<nb-1)
+        {
+            tab[i]=tab[i+1];
+            i++;
+        }
+        tab[nb-1]=NULL; //on a décalé, on met l'ancien dernier à NULL vu qu'on diminue la taille du tableau
+        nb--;
+
+        if (note1->getActive() == false)
+        {
+            if(!RelationManager::getInstance().verifNoteRef(note1)) //la note n'est plus en couple nulle part
+            {
+                QMessageBox::StandardButton reponse;
+                reponse=QMessageBox::question(0,"Supprimer de note", "La note est archivée et n'est plus référencée, voulez-vous  la supprimer ?",QMessageBox::Yes|QMessageBox::No);
+                if(reponse == QMessageBox::Yes)
+                    delete note1;
+                // fait apparaitre une fenêtre de dialogue avec l’utilisateur
+            }
+        }
+        if (note2->getActive() == false)
+        {
+            if(!RelationManager::getInstance().verifNoteRef(note2)) //la note n'est plus en couple nulle part
+            {
+                QMessageBox::StandardButton reponse;
+                reponse=QMessageBox::question(0,"Supprimer de note", "La note note2 est archivée et n'est plus référencée, voulez-vous  la supprimer ?",QMessageBox::Yes|QMessageBox::No);
+                if(reponse == QMessageBox::Yes)
+                    delete note2;
+                // fait apparaitre une fenêtre de dialogue avec l’utilisateur
+            }
+        }
+    }
+}
+
+
+
+void Relation::SeeRelation()
+{
+    std::cout<<"titre = "<<titre.toStdString().c_str()<<"\n"<<"description = "<<description.toStdString().c_str()<<"\n"<<"orientation = "<<orientee<<"\n";
+    Relation::const_iterator it=begin();
+    Relation::const_iterator it_end=end();
+    if(it!=it_end)
+    {
+        do
+        {
+            std::cout<<"label : "<<const_cast<Couple*>(it.elementCourant())->getLabel()<<"\n"<<"note1 : "<<const_cast<Couple*>(it.elementCourant())->getIdNote1().toStdString().c_str()<<"\n"<<"note 2 : "<<const_cast<Couple*>(it.elementCourant())->getIdNote2().toStdString().c_str()<<"\n";
+            it++;
+        } while(it!=it_end);
+    }
+}
+
+
+void Relation::editer()
+{
+    unsigned int rep;
+    std::string titre, desc;
+    std::cout<<"changer titre ? 1 pour oui\n";
+    std::cin>>rep;
+    if (rep==1)
+    {
+        std::cout<<"donner le titre voulu\n";
+        std::cin>>titre;
+        setTitre(QString::fromStdString(titre));
+    }
+    std::cout<<"changer la description\n";
+    std::cin>>rep;
+    if(rep==1)
+    {
+        std::cout<<"donner la nouvelle description \n";
+        std::cin>>desc;
+        setDesc(QString::fromStdString(desc));
+    }
 }

0d2d99640f7e6bf41f1c14d689b1f8e35002f592
Camille <camille.dresse@gmail.com>
2017-06-11 17:19:27 +0200

    Create RelationManager.cpp

--- RelationManager.cpp
+++ RelationManager.cpp
@@ -112,3 +112,105 @@ void RelationManager::save() const {
     stream.writeEndDocument();
     newfile.close();
 }
+
+/*
+void RelationManager::load() {
+    QFile fin("relations.xml");
+    // If we can't open it, let's show an error message.
+    if (!fin.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        throw NotesException("Erreur ouverture fichier notes");
+    }
+    // QXmlStreamReader takes any QIODevice.
+    QXmlStreamReader xml(&fin);
+    //qDebug()<<"debut fichier\n";
+    // We'll parse the XML until we reach end of it.
+    while(!xml.atEnd() && !xml.hasError()) {
+        // Read next element.
+        QXmlStreamReader::TokenType token = xml.readNext();
+        // If token is just StartDocument, we'll go to next.
+        if(token == QXmlStreamReader::StartDocument) continue;
+        // If token is StartElement, we'll see if we can read it.
+        if(token == QXmlStreamReader::StartElement) {
+            // If it's named taches, we'll go to the next.
+            if(xml.name() == "relations") continue;
+            // If it's named tache, we'll dig the information from there.
+            if(xml.name() == "relation") {
+                qDebug()<<"new relation\n";
+                QString titre;
+                QString description;
+                bool orientee;
+                unsigned int nbc;
+                unisgned int nbcmax;
+                Couple** c;
+                QXmlStreamAttributes attributes = xml.attributes();
+                xml.readNext();
+                //We're going to loop over the things because the order might change.
+                //We'll continue the loop until we hit an EndElement named article.
+                while(!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "relation")) {
+                    if(xml.tokenType() == QXmlStreamReader::StartElement) {
+                        // We've found identificteur.
+                        if(xml.name() == "titre") {
+                            xml.readNext(); titre=xml.text().toString();
+                            qDebug()<<"titre="<<titre<<"\n";
+                        }
+
+                        // We've found description.
+                        if(xml.name() == "description") {
+                            xml.readNext(); description=xml.text().toString();
+                            qDebug()<<"descritpion="<<description<<"\n";
+                        }
+                        // We've found orientee
+                        if(xml.name() == "orientee") {
+                            xml.readNext();
+                            orientee=(xml.text().toString()).toInt();
+                            qDebug()<<"orientee="<<orientee<<"\n";
+                      
+                        // We've found nbCouple
+                        if(xml.name() == "nbCouple") {
+                            xml.readNext();
+                            nbc=(xml.text().toString()).toInt();
+                            qDebug()<<"nb="<<nbc<<"\n";
+                        }
+                        // We've found nbMaxVersion
+                        if(xml.name() == "nbMaxCouple") {
+                            xml.readNext();
+                            nbcmax=(xml.text().toString()).toInt();
+                            qDebug()<<"max="<<nbcmax<<"\n";
+                        }
+                        /*
+                         while(!(xml.tokenType() == QXmlStreamReader::EndElement && (xml.name() == "article"||xml.name() == "tache"||xml.name() == "multimedia"))) {
+                         if(xml.tokenType() == QXmlStreamReader::StartElement) {
+                        // We've found dateversion
+                        if(xml.name() == "date version") {
+                            xml.readNext();
+                            dateMod.fromString(xml.text().toString());
+                            qDebug()<<"dateModif="<<dateMod<<"\n";
+                        }
+                         }
+                         }
+                        // We've found versions
+                        if(xml.name() == "nb max version") {
+                            xml.readNext();
+                            nbMV=xml.text().toString();
+                            qDebug()<<"nbMaxVersion="<<nbMV<<"\n";
+                        }
+                        // A COMPLETER POUR REMPLIR VERSION
+
+                    }
+                    // ...and next...
+                    xml.readNext();
+                }
+                qDebug()<<"ajout note "<<identificateur<<"\n";
+                addNoteXML(identificateur,titre,dateC,dateMod,act,supp,nbV,nbMV,v);
+            }
+        }
+    }
+    // Error handling.
+    if(xml.hasError()) {
+        throw NotesException("Erreur lecteur fichier notes, parser xml");
+    }
+    // Removes any device() or data from the reader * and resets its internal state to the initial state.
+    xml.clear();
+    qDebug()<<"fin load\n";
+}
+*/

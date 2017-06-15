f6d8b75487fe38b66341a135aa45588fa752058f
lucieWartelle <lucie.wartelle@laposte.net>
2017-06-15 11:24:29 +0200

    Update RelationManager.h

--- RelationManager.h
+++ RelationManager.h
@@ -1,91 +1,98 @@
-#ifndef RELATIONMANAGER_H
-#define RELATIONMANAGER_H
+#ifndef NOTEMANAGER_H
+#define NOTEMANAGER_H
 
-#include <ctime>
-#include <QString>
-#include <QtXml>
 #include <QFile>
-#include "relation.h"
+#include <QTextCodec>
+#include <QtXml>
+#include <QMessageBox>
+#include <QString>
+#include "note.h"
+#include "version.h"
+#include "relationmanager.h"
 
 /**
- * @brief la classe RelationManager
- * gère l'ensemble des objets Relation
+ * @brief La classe NotesException
  */
-class RelationManager
-{
-    static RelationManager* managR; /**< pointeur sur l'unique instance */
-    Relation** relations; /**< tableau de l'ensemble des relations */
-    unsigned int nbRelations; /**< entier, nombre de relations stockées dans relations */
-    Relation* Reference; /**< pointeur sur la relation spéciale Reference */
-    unsigned int nbMaxRelations; /**< entier, capacité actuelle du tableau relations */
-    /**
-     * @brief constructeur sans argument de RelationManager
-     */
-    RelationManager():nbRelations(0),nbMaxRelations(0),relations(nullptr){
-         this->Reference = new Relation("Reference", "Relation speciale reference", true);
-    }
-    /**
-      @brief destructeur de RelationManager
-      détruit l'ensemble des objets Relation, et libère
-      l'espace mémoire alloué au tableau relations
-      */
-    ~RelationManager(){
-        for(unsigned int i=0; i<nbRelations; i++) delete relations[i]; // composition uniquement
-        delete[] relations; // composition + agrégation
-    }
-    /**
-     * @brief constructeur de recopie de RelationManager
-     * en privé car on ne veut pas qu'il puisse être utilisé
-     */
-    RelationManager(const RelationManager& m);
-    /**
-     * @brief operateur d'affectation de RelationManager
-     * en privé car on ne veut pas qu'il puisse être utilisé
-     */
-    RelationManager& operator=(const RelationManager& m);
 
+class NotesException{
+public:
+    /**
+     * @brief Un constructeur de NotesException
+     * Attribut le paramètre message à l'attribut info
+     * @param message de type const QString&
+     */
+    NotesException(const QString& message):info(message){}
+    /**
+     * @brief accesseur getInfo
+     * @return le QString correspondant à l'attribut privé info
+     */
+    QString getInfo() const {return info;}
+private:
+    /**
+     * @brief info attribut de type QString
+     * correspond au message qui sera envoyé lors de l'exception
+     */
+    QString info;
+};
+
+
+/**
+ * @brief La classe NoteManager
+ * gère les objets Note
+ */
+class NoteManager
+{
+    static NoteManager* managN; /**< pointeur sur l'unique instance, statique, de NoteManager */
+    Note** notes;  /**< tableau de l'ensemble des Notes */
+    unsigned int nbNotes; /**< entier, nombre de Notes existant pour le moment */
+    unsigned int nbMaxNotes; /**< entier, capacité actuelle du tableau notes */
+    /**
+     * @brief constructeur sans argument de NoteManager
+     */
+    NoteManager():nbNotes(0), nbMaxNotes(0), notes(nullptr){
+    }
+    /**
+      *@brief Destructeur de NoteManager
+      * libère l'espace mémoire alloué au tableau de Notes, et détruit l'ensemble des notes
+    */
+    ~NoteManager()
+    {
+        for(unsigned int i=0; i<nbNotes; i++)
+            delete notes[i];
+    }
+    /**
+     * @brief constructeur par recopie de NoteManager
+     * en privé car on ne veut pas qu'il puisse être utilisé
+     */
+    NoteManager(const NoteManager& m);
+    /**
+     * @brief operateur d'affectation
+     * en privé car on ne veut pas qu'il puisse être utilisé
+    */
+    NoteManager& operator=(const NoteManager& m);
 
 public :
-    /**
-     * @brief methode editerRelation
-     * @param r pointeur sur la Relation à éditer
-     * @param t le titre que l'on souhaite donner
-     * @param d la description que l'on souhaite attribuer à la relation
-     */
-    void editerRelation(Relation* r, QString& t, QString& d){r->editer(t,d);}
-    /**
-     * @brief methode suppRelation
-     * @param r, reference sur l'objet relation à supprimer
-     */
-    void suppRelation(Relation& r);
-    /**
-     * @brief methode addRelation
-     * @param t un QString& correspondant au titre que l'on souhaite donner
-     * @param d QString& correspondant à la description de la Relation à ajouter
-     * @param orient bool mis à true par défaut
-     */
-    void addRelation(QString& t, QString& d, bool orient=true);
 
     /**
-     * @brief La classe Iterator de RelationManager
+     * @brief La classe Iterator de NoteManager
      */
     class Iterator
     {
-        friend class RelationManager;
-        Relation** currentR; /**< le tableau d'objets Relation dans lequel itérer */
-        unsigned int nbRemain; /**< entier, nombre de relations à parcourir avant d'atteindre la fin du tableau */
+        friend class NoteManager;
+        Note** currentN; /**< tableau de Note dans lequel on se déplace */
+        unsigned int nbRemain; /**< entier, nombre de Notes à parcourir avant d'atteindre la fin du tableau */
         /**
          * @brief constructeur de Iterator
-         * @param r tableau de pointeurs d'objets Reference
-         * @param nb entier, nombre d'objets Référence à parcourir avant d'atteindre la fin du tableau
+         * @param n tableau de pointeurs de Note
+         * @param nb entier correspondant au nombre de notes dans n
          */
-        Iterator(Relation** r, unsigned nb):currentR(r),nbRemain(nb){}
+        Iterator(Note** n, unsigned nb):currentN(n),nbRemain(nb){}
 
     public:
         /**
          * @brief constructeur sans argument de Iterator
          */
-        Iterator():nbRemain(0),currentR(nullptr){}
+        Iterator():currentN(nullptr),nbRemain(0){}
         /**
          * @brief methode isDone
          * @return true si on a atteint la fin du tableau, false sinon
@@ -93,73 +100,129 @@ public :
         bool isDone() const { return nbRemain==0; }
         /**
          * @brief methode next
-         * passe à la relation suivante dans le tableau
+         * sert à passer à la note suivante
          */
         void next()
         {
             if (isDone())
                 throw NoteException("error, next on an iterator which is done");
             nbRemain--;
-            currentR++;
+            currentN++;
         }
         /**
          * @brief methode current
-         * @return une reference sur la Relation actuellement pointée
+         * @return une reference sur la Note sur laquelle on est actuellement avec l'iterateur
          */
-        Relation& current() const
+        Note& current() const
         {
             if (isDone())
                 throw NoteException("error, indirection on an iterator which is done");
-            return **currentR;
+            return **currentN;
         }
     };
 
     /**
      * @brief methode getIterator
-     * @return un iterateur sur la debut du tableau relations
+     * @return un objet Iterator au debut du tableau de notes
      */
-    Iterator getIterator() {return Iterator(relations,nbRelations);/*0*/}
+    Iterator getIterator()
+    {
+        return Iterator(notes,nbNotes);
+    }
+
     /**
      * @brief methode statique getInstance
-     * créé l'unique instance de RelationManager si elle n'existe pas déjà
-     * @return une reference sur l'unique instance de RelationManager
+     * @return l'instance unique de NoteManager, et la créé si elle n'existe pas encore
      */
-    static RelationManager& getInstance()
+    static NoteManager& getInstance()
     {
-        if(!managR)
-            managR= new RelationManager();
-        return *managR;
+        if(!managN)
+            managN=new NoteManager();
+        return *managN;
     }
+
     /**
-     * @brief methode statique free_instance
+     * @brief methode statique liberer_instance
+     * supprime l'unique instance de NoteManager
      */
-    static void free_instance(){if (managR) delete managR;}
+    static void liberer_instance(){if (managN) delete managN;}
     /**
-     * @brief methode getNbRelations
-     * @return le nombre de relation dans RelationManager
+     * @brief methode addNote
+     * créé une note et la range dans le tableau notes
+     * @param id de type QString
+     * @param title de type QString
+     * @param crea de type QDateTime
+     * @param modif de type QDateTime
+     * @param v une reference sur un objet version
      */
-    int getNbRelations(){return nbRelations;}
+    void addNote(QString id, QString title, QDateTime crea,QDateTime modif, const Version& v);
+
     /**
-     * @brief methode getRef
-     * @return un pointeur sur la relation spéciale Reference
+     * @brief methode addNote
+     * @param id l'id desiré de la note à ajouter
+     * @param title le titre de la note à ajouter
      */
-    Relation* getRef(){return Reference;}
+    void addNote(QString id, QString title);
     /**
-     * @brief methode verifNoteRef
-     * @param n un pointeur sur une note
-     * @return true si la note est référencée, false sinon
+     * @brief methode addNoteXML
+     * créé une note et la range dans le tableau notes
+     * @param id de type QString
+     * @param title de type QString
+     * @param crea de type QDateTime
+     * @param modif de type QDateTime
+     * @param act de type bool, statut active de la note
+     * @param supp de type bool, statut supprime de la Note
+     * @param nbV de type unisgned int, nombre de versions de la note
+     * @param nbMV de type unisgned int, nombre de versions maximum de la note
+     * @param a de type QDateTime
+     * @param v un tableau d'adresses de versions
      */
-    bool verifNoteRef(const Note* n);
-     /**
-     * @brief sauve les relations dans un fichier xml
+    void addNoteXML(QString id, QString title, QDateTime crea,QDateTime modif,bool act, bool supp, unsigned int nbV, unsigned int nbMV,  Version** v);
+    /**
+     * @brief methode editer
+     * @param n pointeur sur la note à éditer
+     * @param title de type QString, nouveau titre
+     * @param modif de type QDateTime, la date de la modification
+     * @param v une reference sur un objet Version
+     */
+    void editer(Note* n, QString title, QDateTime modif, const Version& v);
+    /**
+     * @brief methode supprimerNote
+     * l'attribut active de l'objet Note est mis à false si la note est référencée
+     * l'attribut supprime est mis à true sinon
+     * @param n reference sur l'objet Note à supprimer
+     */
+    void supprimerNote(Note& n);
+    /**
+     * @brief methode viderCorbeille
+     * supprime définitivement tous les objets Note dont l'attribut supprime est à true
+     */
+    void viderCorbeille();
+    /**
+     * @brief accesseur getNbNotes
+     * @return un entier correspondant aux nombre de notes stockées dans le tableau notes
+     */
+    unsigned int getNbNotes(){return nbNotes;}
+    /**
+     * @brief accesseur getNbMaxNotes
+     * @return un entier correspondant à la capacité actuelle du tableau notes
+     */
+    unsigned int getNbMaxNotes(){return nbMaxNotes;}
+    /**
+     * @brief accesseur getNote
+     * @param id de type QString
+     * @return un pointeur sur la note dont l'ID est id
+     */
+    Note* getNote(QString id);
+    /**
+     * @brief sauve les notes dans un fichier xml
      */
     void save() const;
     /**
-     * @brief telecharge les relations depuis un fichier xml
+     * @brief telecharge les notes depuis un fichier xml
      */
     void load();
 
 };
 
-
-#endif // RELATIONMANAGER_H
+#endif // NOTEMANAGER_H
